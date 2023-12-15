22620551fd60c46a1a846e4af3a51de1f207a66b
JacobC <dhmt52@durham.ac.uk>
2023-12-15 17:22:01 +0000

    Navbar + Temp Content Grid - Draft #3

--- client.js
+++ client.js
@@ -1,8 +1,10 @@
 const artistsSection = document.getElementById("artistsSec");
 const toggleButton = document.getElementById("toggleUPDOWN");
+const content = document.getElementById("content");
+
 
 toggleButton.addEventListener("click", () => {
 	artistsSection.classList.toggle("artist-hidden");
 	toggleButton.classList.toggle("arrow-change");
+	content.classList.toggle("content-change");
 });
-
