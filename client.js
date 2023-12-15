c54575be1c985b8bff82a6f1ad004fa5f8c954a8
JS Chau <jacob.chauu@gmail.com>
2023-12-15 19:43:07 +0000

    Dynamic Grid Fixed

--- client.js
+++ client.js
@@ -3,8 +3,27 @@ const toggleButton = document.getElementById("toggleUPDOWN");
 const content = document.getElementById("content");
 
 
+
 toggleButton.addEventListener("click", () => {
 	artistsSection.classList.toggle("artist-hidden");
 	toggleButton.classList.toggle("arrow-change");
-	content.classList.toggle("content-change");
+	adjustMainSectionHeight();
 });
+
+function adjustMainSectionHeight() {
+	const navbarHeight = document.querySelector('nav').offsetHeight;
+	const artistHeight = artistsSection.offsetHeight;
+	const windowHeight = window.innerHeight;
+	
+	let availableHeight = windowHeight - navbarHeight;
+  
+	if (!artistsSection.classList.contains('artist-hidden')) {
+	  availableHeight -= artistHeight;
+	}
+  
+	content.style.height = `${availableHeight}px`;
+  }
+  
+  window.addEventListener('load', adjustMainSectionHeight);
+
+  
