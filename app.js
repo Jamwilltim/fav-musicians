333f216a6ae0e6ae8c212e05e410a193c169713b
JS Chau <jacob.chauu@gmail.com>
2023-12-29 21:36:10 +0000

    Server-side loading json data after Button click (Next Steps - Comments Section)

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const app = express();
-const fs = require('fs');
 
 app.use(express.static('client-side'));
 app.use(express.json());
 
+const artistinfo = require('./artist-info.json')
 
+app.get('/artist', function(req,resp){
+    let key = req.query.temp; 
+    for (let artist of artistinfo){
+        if (artist.ArtistName == key){
+            resp.send(artist);
+        }
+    }
 
-
-
+});
 
 module.exports = app;
 
--- client-side/client.js
+++ client-side/client.js
@@ -1,30 +1,32 @@
-const artistsSection = document.getElementById("artistsSec");
-const toggleButton = document.getElementById("toggleUPDOWN");
-const content = document.getElementById("content");
+document.querySelectorAll('.Artist-Button').forEach(button => {
+    button.addEventListener('click', async function (event) {
+        event.preventDefault();
+        const artistName = this.getAttribute('id');
+        try {
+            let response = await fetch(`http://127.0.0.1:8080/artist?temp=${artistName}`);
+            let body = await response.json(); 
+            const {ArtistName, Card1, Card2, Card3A, Card4, Quote, CoverImage, SpotifyUrl} = body
+            document.getElementById('Card1-front').querySelector('h1').innerHTML = `Who is ${ArtistName}?`;
+            document.getElementById('Card1-back').innerHTML = Card1;
+            document.getElementById('Card2-back').innerHTML = Card2;
+            document.getElementById('Card1-back').style.padding = '10px';
+            document.getElementById('Card2-back').style.padding = '10px';
+            document.getElementById('Card3-back').querySelector('ul').innerHTML = makeList(Card3A);
+            document.getElementById('Card4-back').querySelector('ul').innerHTML = makeList(Card4);
+            document.getElementById('ArtistNameHeader').innerHTML = ArtistName;
+            document.getElementById('artistImage').src = CoverImage;
+            document.getElementById('Quote').innerHTML = Quote;
+            document.getElementById('SpotifyPlaylist').src = SpotifyUrl;
+        } catch (error) {
+            alert(error);
+        }
+    })
+})
 
-
-
-toggleButton.addEventListener("click", () => {
-	artistsSection.classList.toggle("artist-hidden");
-	toggleButton.classList.toggle("arrow-change");
-	adjustMainSectionHeight();
-});
-
-function adjustMainSectionHeight() {
-	const navbarHeight = document.querySelector('nav').offsetHeight;
-	const artistHeight = artistsSection.offsetHeight;
-	const windowHeight = window.innerHeight;
-	
-	let availableHeight = windowHeight - navbarHeight;
-  
-	if (!artistsSection.classList.contains('artist-hidden')) {
-	  availableHeight -= artistHeight;
-	}
-  
-	content.style.height = `${availableHeight}px`;
-  }
-  
-window.addEventListener('load', adjustMainSectionHeight);
-window.addEventListener('resize', adjustMainSectionHeight);
-
-
+function makeList(givenlist){
+    let listcontent = '';
+    for (let album of givenlist) {
+        listcontent += `<li>${album}</li>`;
+    }
+    return listcontent;
+}
